1321c5600740216c5c616790fd4893f6ebda69a6
Peeddrro <pedroh.araujo7784@gmail.com>
2025-04-04 20:12:57 +0000

    Atualizacao Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,45 +1,66 @@
 #include <stdio.h>
 
+void Rainha(int rainha) {
+    if (rainha > 0){
+        printf("Esquerda\n");
+        Rainha(rainha - 1);
+    }
+}
+
+void Torre(int torre){
+    if (torre > 0){
+        printf("Direita\n");
+        Torre(torre - 1);
+    }
+}
+
+
+int bispoh, bispov; // h = horizontal e v = vertical
+void Bispo(){
+    for (bispov = 0; bispov < 1; bispov++){
+        for (bispoh = 0; bispoh < 5; bispoh++){
+            printf("Cima e Direita\n");
+        }
+        printf("\n");
+    }
+}
+
 int main(){
 
-    int rainha = 0, bispo = 0, torre = 0;
+    //Rainha
+    printf("\nRainha se moveu para\n");
+    Rainha(8); //Chama Rainha com valor oito e vai subtraindo 1 a cada auto chamada
 
-    //Programa da Rainha
-    printf("\nMovimento da Rainha\n");
-
-    for (rainha; rainha < 8; rainha++){
-        printf("Rainha se moveu para a Esquerda\n");
-    }
-    
-    //Programa da Torre
-    printf("\nMovimento da Torre\n");
-
-    while(torre < 5){
-        printf("Torre se moveu para a Direita\n");
-        torre++;
-    } 
-
-    //Programa do Bispo
-    printf("\nPrograma do Bispo\n");
-    do{
-        printf("Bispo se moveu para Cima\n");
-        printf("Bispo se moveu para Direita\n");
-        bispo++;
-
-    } while (bispo < 5);
+    //Torre
+    printf("\nTorre se moveu para\n");
+    Torre(5);
+  
+    //Bispo
+    printf("\nBispo se moveu para\n");
+    Bispo();
 
     //Programa do Cavalo
-    int move = 0;
+    int cavaloh, cavalov;
 
     printf("\nPrograma do Cavalo\n");
 
-    do{
-        for(int i = 0; i < 2; i++){
-            printf("Cavalo se moveu para Baixo\n");
+   for(cavaloh = 0; cavaloh < 7; cavaloh++){ // Adicionei um valor mais alto, pois foi a maneira que encontrei para dar utilidade a comparacao
+    for(cavalov = 0; cavalov < 7 ; cavalov++){ // Um loop dentro do loop. O loop externo faz a inicializacao, entao o loop interno roda completamente, apos isso o externo atualiza e se a condicao ainda for verdadeira o interno ira reiniciar, executando tudo novamente.
+        printf("Cima\n");
+        if (cavalov == 1 && cavaloh == 0){
+            break;
         }
-        printf("Cavalo se moveu para Esquerda\n");
-    } while(move--);
-    
+        
+    } 
+    printf("Direita\n");    
+    if (cavalov <= 1){
+        break;
+    }                 
+              
 
+    }
+
+    
+    
     return 0;
 }
